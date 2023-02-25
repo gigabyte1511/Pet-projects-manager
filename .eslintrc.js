40dafef0a2253bc6fe45c90146970fa9f260550d
gigabyte1511 <gigbyte1511@gmail.com>
2023-02-25 23:33:53 +0600

    Create project structure, add MUI

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,5 +25,10 @@ module.exports = {
     'no-console': 0,
     'import/no-cycle': 0,
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'no-unused-vars': 'off',
+    'import/prefer-default-export': [
+      ('off'),
+      { target: 'any' },
+    ],
   },
 }
--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import logo from './logo.svg'
 import './App.css'
+import {
+  Container, createTheme, CssBaseline, ThemeProvider, useMediaQuery,
+} from '@mui/material'
+import { useMemo } from 'react'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Main from './components/Main/Main'
 
 function App() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+
+  const theme = useMemo(
+    () => createTheme({
+      palette: (prefersDarkMode) ? ({
+        mode: 'dark',
+        primary: { main: '#0288d1', dark: '#ffa726' },
+      }) : ({
+        mode: 'light',
+        primary: { main: '#0288d1', dark: '#ffa726' },
+        background: { default: 'rgba(230, 230, 230, 0.7)' },
+      }),
+    }),
+    [prefersDarkMode],
+  )
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header />
+      <Main />
+      <Footer />
+    </ThemeProvider>
   )
 }
 
